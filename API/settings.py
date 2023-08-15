1935b7c02b12b281e6595432576db1dc9822e48c
davis45368 <dhj314159@gmail.com>
2023-08-15 16:34:59 -0500

    se agrega la url del frontend

--- API/settings.py
+++ API/settings.py
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Acturizacion de cors
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "https://clientes-frontend-react.vercel.app/clientes"]
+CORS_ALLOWED_ORIGINS = ["https://clientes-frontend-react.vercel.app/clientes"]
 
 # Documentación
 
